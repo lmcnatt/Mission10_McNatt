892a3558d729f77f721f559f9a287e661d261530
Logan McNatt <logan.m.mcnatt@gmail.com>
2024-03-09 21:55:35 -0700

    nice

--- backendApi/obj/Debug/net8.0/backendApi.AssemblyInfo.cs
+++ backendApi/obj/Debug/net8.0/backendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45aff93894c6adcfb025f65c6293f6a246c1fd5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96129c1bb2aa6329b5ec3d6f05c46e7e8bb8ca56")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
